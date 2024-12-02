6c8428e82d2e1f80c3bbc9751789c90f6f66f088
Pixsl-Studio <automation@pixsl.studio>
2024-12-02 13:42:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def generate_coupon():
     coupon = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-    return jsonify({coupon})
+    return jsonify({"coupon_code": coupon})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
